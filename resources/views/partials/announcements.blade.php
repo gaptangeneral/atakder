c79a6c476dd831c22f632f187b0c71a58cac80cf
gaptangeneral <67544217+gaptangeneral@users.noreply.github.com>
2025-08-08 09:49:51 +0300

    Add files via upload

--- resources/views/partials/announcements.blade.php
+++ resources/views/partials/announcements.blade.php
@@ -1,9 +1,19 @@
 <!-- Duyurular Section -->
+@php
+    $activeAnnouncements = \App\Models\Announcement::where('is_active', true)
+        ->orderBy('order', 'asc')
+        ->get();
+@endphp
+
+@if($activeAnnouncements->count() > 0)
 <div class="bg-secondary text-white py-2 overflow-hidden">
     <div class="flex scroll-left whitespace-nowrap">
-        <span class="mx-4"><i class="fas fa-bullhorn mr-2"></i> Duyuru 1: Afet bölgelerine yardım malzemeleri gönderildi</span>
-        <span class="mx-4"><i class="fas fa-bullhorn mr-2"></i> Duyuru 2: Arama kurtarma ekibimiz 7/24 hizmetinizde</span>
-        <span class="mx-4"><i class="fas fa-bullhorn mr-2"></i> Duyuru 3: Bağışlarınızla daha fazla kişiye ulaşıyoruz</span>
-        <span class="mx-4"><i class="fas fa-bullhorn mr-2"></i> Duyuru 4: Deprem eğitimlerimiz devam ediyor</span>
+        @foreach($activeAnnouncements as $announcement)
+            <span class="mx-4">
+                <i class="fas fa-bullhorn mr-2"></i> 
+                {{ $announcement->title }}: {{ Str::limit(strip_tags($announcement->content), 100) }}
+            </span>
+        @endforeach
     </div>
 </div>
+@endif
